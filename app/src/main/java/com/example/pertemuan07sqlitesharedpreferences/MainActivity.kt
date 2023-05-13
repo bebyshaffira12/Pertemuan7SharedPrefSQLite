dba14d53a6fed9a0d10ec47b9682ab8882babe92
bebyshaffira12 <beby26shaffira@gmail.com>
2023-05-13 19:10:04 +0700

    Initial commit
    Signed-off-by: bebyshaffira12 <beby26shaffira@gmail.com>

---
+++ app/src/main/java/com/example/pertemuan07sqlitesharedpreferences/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.pertemuan07sqlitesharedpreferences
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btnLogout: Button = findViewById(R.id.btnLogout)
+        val savedLogin = getSharedPreferences("Login", MODE_PRIVATE)
+        val editSavedLogin = savedLogin.edit()
+
+        //ketika button logout diklik, dia akan melakukan penyimpanan perubahan data
+        //dan menuju ke login
+        btnLogout.setOnClickListener {
+            editSavedLogin.putString("Email", null)
+            editSavedLogin.putString("Password", null)
+            editSavedLogin.putString("Status", "Off")
+            editSavedLogin.commit()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
